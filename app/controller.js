23a343a029ed78f42fd69492d5afbcd3934e0061
Rayhan Shikder <rayhanshikder@Rayhans-MBP-2.hitronhub.home>
2017-11-06 00:05:08 -0600

    boilerplate added and connection established with node backend

---
+++ app/controller.js
@@ -0,0 +1,39 @@
+/**
+ * Main application controller
+ *
+ * You can use this controller for your whole app if it is small
+ * or you can have separate controllers for each logical section
+ * 
+ */
+;(function() {
+
+  angular
+    .module('boilerplate')
+    .controller('MainController', MainController);
+
+  MainController.$inject = ['LocalStorage', 'QueryService'];
+
+
+  function MainController(LocalStorage, QueryService) {
+
+    // 'controller as' syntax
+    var self = this;
+
+
+    ////////////  function definitions
+
+
+    /**
+     * Load some data
+     * @return {Object} Returned object
+     */
+    QueryService.query('GET', 'videos', {}, {})
+      .then(function(ovocie) {
+        self.ovocie = ovocie.data;
+        console.log('response is');
+        console.log(self.ovocie);
+      });
+  }
+
+
+})();
